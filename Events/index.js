7384c7cfe0edfbfc823d91a6a4516312430b8fef
Assane <74298624+Asmalick@users.noreply.github.com>
2022-03-24 10:44:34 +0100

    ajout d'autres évenements

--- Events/index.js
+++ Events/index.js
@@ -48,3 +48,34 @@ function  faireQuelqueChose() {
      //console.log(e.target.value);
  //    vertical.innerHTML = e.target.value
 // });
+
+//********************************* */
+
+const theme = document.querySelectorAll('.theme');
+/*const dark = document.querySelector('.dark');
+const salmon = document.querySelector('salmon');
+const yellow = document.querySelector('.yellow'); */
+
+theme.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        //console.log(yes);
+        console.log(e.target.id);
+        document.body.classList.remove("darkTheme", "salmonTheme", "yellowTheme");
+        switch (e.target.id) {
+            case "dark":
+                document.body.classList.add("darkTheme");
+                break;
+            case "salmon":
+                document.body.classList.add("salmonTheme");
+                break;
+            case "yellow":
+                document.body.classList.add("yellowTheme");
+                break;
+            default:
+                null;
+        }
+
+
+
+    })
+})
